3c0d9975d33b3f23a673063093cc54967743aa37
brianhkang1@gmail.com <brianhkang1@gmail.com>
2018-07-09 20:19:37 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -14,4 +14,25 @@ class TicTacToe
   [0, 4, 8], #Diagonal 1 
   [2, 4, 6], #Diagonal 2 
 ]
+
+  def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end 
+
+def input_to_index(input)
+  input = input.to_i - 1
+end
+
+def move(board, index, current_player)
+  board[index] = current_player
+end
+
+def position_taken?(board, index)
+  !(board[index].nil? || board[index] == " ")
+end
+
 end
